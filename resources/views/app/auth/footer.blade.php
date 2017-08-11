609c7552ed9a8c426d1dd88860b6b864d812f658
xuebingwang <406964108@qq.com>
2017-08-11 15:07:40 +0800

    登录页面脚链接修改

--- resources/views/app/auth/footer.blade.php
+++ resources/views/app/auth/footer.blade.php
@@ -1,10 +1,10 @@
 <section class="footer">
     <div class="item-a-warp">
-        <a target="_blank" href="/about/me" class="item-btn">关于我们</a>
+        <a target="_blank" href="javascript:" class="item-btn">关于我们</a>
         {{--<a target="_blank" href="/about/download" class="item-btn">下载客户端</a>--}}
-        <a target="_blank" href="/about/privacy" class="item-btn">隐私</a>
-        <a target="_blank" href="/about/terms" class="item-btn">条款</a>
-        <a target="_blank" href="http://www.lagou.com/center/company_23770.html" class="item-btn">招聘</a>
+        <a target="_blank" href="javascript:" class="item-btn">隐私</a>
+        <a target="_blank" href="javascript:" class="item-btn">条款</a>
+        <a target="_blank" href="javascript:" class="item-btn">招聘</a>
         {{--<span class="item-btn">
                 <select class="lange">
                         <option value="zh-cn" selected=selected class="zh-cn">简体中文</option>
