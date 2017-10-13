4cb78227ac84275501bb5e687139bfe7d1d8a922
xuebingwang <406964108@qq.com>
2017-10-13 17:25:29 +0800

    样式修改

--- resources/views/app/common/nav.blade.php
+++ resources/views/app/common/nav.blade.php
@@ -5,19 +5,19 @@
     ?>
     <a href="/" class="f1 @if($segment == '' || $segment == 'index') on @endif">
         <span class="icon icon-home"></span>
-        <p>首页</p>
+        <span>首页</span>
     </a>
     <a href="{{route('art_circle_recommend')}}" class="f2 @if($segment == 'art_circle') on @endif">
         <span class="icon icon-process"></span>
-        <p>圈子</p>
+        <span>圈子</span>
     </a>
-    <a href="http://mall.huapinhua.com/app/index.php?i=3&c=entry&m=ewei_shopv2&do=mobile" class="f3">
+    <a href="http://mall.cateyeart.com/app/index.php?i=3&c=entry&m=ewei_shopv2&do=mobile" class="f3">
         <span class="icon icon-shop"></span>
-        <p>商城</p>
+        <span>商城</span>
     </a>
     <a href="{{route('member_index')}}" class="f4 @if($segment == 'member') on @endif">
         <i>3</i>
         <span class="icon icon-person2"></span>
-        <p>我的</p>
+        <span>我的</span>
     </a>
 </div>
