f90b9809095497945ef5aac4efb26d77002ff7ec
xuebingwang <406964108@qq.com>
2019-12-11 21:04:45 +0800

    文字修改

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -105,19 +105,14 @@
             })
         });
 
-
-        <?php if (isset($show) && $show == 'cooperate_apply'):?>
-        show_cooperate_apply();
-        <?php endif;?>
-
         var jiameng_tip = '备注：申请通过后，猫眼艺术会由专人负责联系合作事宜，也可以将资料发到邮箱2594134988@qq.com即可。';
         function show_cooperate_apply() {
             $.modal({
-                title:  '免费办展申请',
+                title:  '加盟合作',
                 text: '' +
                 '<div style="font-size: 12px;text-align: left; background: rgb(220,220,220); border-radius: .35rem; padding: .2rem;">' +
                 '<p>欢迎关注画品画，猫眼艺术是一家VR艺术传播教育体验服务商，免费为艺术家和少儿美术培训机构提供VR艺术展、艺术直播、艺术教育学习，提供用户在线看展、学习、在线购买作品。</p>' +
-                '<p>加盟商需是服务于美术行业，个人、机构、协会群体，由加盟商负责线下美术爱好者教育，培训组织猫眼艺术提供线上VR展览推广，建立线上线下结合服务于美术事业，详细加盟合作模式请提交资料后我方由专人负责联系开通。</p>' +
+                '<p>加盟商需是服务于美术行业，个人、机构、学校、协会群体，由加盟商负责线下美术爱好者教育，培训组织猫眼艺术提供线上VR展览推广，建立线上线下结合服务于美术事业，详细加盟合作模式请提交资料后我方由专人负责联系开通。</p>' +
                 '<img src="/cateyeart/v2/images/133.png" width="100%">' +
                 '</div>',
                 verticalButtons: true,
@@ -136,5 +131,9 @@
                 ]
             })
         }
+
+        <?php if (isset($show) && $show == 'cooperate_apply'):?>
+        show_cooperate_apply();
+        <?php endif;?>
     </script>
 @endsection
